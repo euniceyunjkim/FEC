410c7a782321718fd70158bda40ea2c56273cff5
alexshiao99 <alexshiao99@gmail.com>
2022-03-31 14:04:45 -0700

    Added a default value with API call and provided it as context

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -3,17 +3,26 @@ import Overview from './Overview/index.jsx';
 import QA from './QA/index.jsx';
 import Related from './Related/index.jsx';
 import Reviews from './Reviews/index.jsx';
+import currentProducts from '../Contexts/CurProdContext.js';
+const axios = require('axios');
 
 const App = ({}) => {
 
+  let [currentProd, setCurrentProd] = useState({});
+
+  useEffect(() => {
+    axios.get('/products')
+    .then((data) => setCurrentProd(data.data[0]))
+    .catch((err) => console.error(err))
+  }, [])
 
   return (
-  <div>
+  <currentProducts.Provider value={{currentProd, setCurrentProd}}>
     <Overview />
     <Related />
-    <Reviews />
     <QA />
-  </div>
+    <Reviews />
+  </currentProducts.Provider>
   )
 };
 
