a8d488da64a5d8fe7eacce7639dd10b39541419b
Eunice Kim <euniceyunjkim@gmail.com>
2022-03-31 11:47:54 -0700

    Make base structure for widget.

--- client/src/Components/Overview/index.jsx
+++ client/src/Components/Overview/index.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
-const Overview = ({}) => {
-
+function Overview() {
   return (
-    <div>Overview</div>
-  )
+    <div>
+      <div>Logo And Search Bar</div>
+      <div id="product">
+        <div id="product gallery">Product Gallery</div>
+        <div id="product details">
+          <div id="review summ">Review Summary</div>
+          <div id="product category">Product Category</div>
+          <h3>Product Name</h3>
+          <h5>$199</h5>
+          <div id="style-box"></div>
+          <div id="checkout-box">
+            <div id="size">Dropdown</div>
+            <div id="quantity">Dropdown</div>
+            <button type="button">Add to Bag</button>
+            <button type="button">Star</button>
+          </div>
+        </div>
+      </div>
+      <div id="product-overview"></div>
+      <div id="share-socials"></div>
+    </div>
+  );
 }
 
 export default Overview;
