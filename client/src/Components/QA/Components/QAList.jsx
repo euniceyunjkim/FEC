b41ed022d5ffb11351daad81bdd06a1b7e116927
Casey Eads <eadscaseyl@gmail.com>
2022-04-01 14:25:41 -0700

    Update framework.

---
+++ client/src/Components/QA/Components/QAList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Questions from "./Questions.jsx";
+import Answers from "./Answers.jsx";
+
+function QAList({}) {
+  return (
+    <div>
+      <Questions />
+      <Answers />
+    </div>
+  );
+}
+
+export default QAList;
