b955f11214e2d3692e7bfe33892d2a1eddb51f63
Casey Eads <eadscaseyl@gmail.com>
2022-04-01 14:24:47 -0700

    Update framework.

---
+++ client/src/Components/QA/Components/Answers.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Answers({}) {
+  return (
+    <div>
+      <ul>
+        <li>This is an answer</li>
+        <li>This is an answer</li>
+      </ul>
+    </div>
+  );
+}
+export default Answers;
