fdcd15b0b13ac8e1102e94bb2ba0a6c91807a6c1
Eunice Kim <euniceyunjkim@gmail.com>
2022-04-02 02:05:00 -0700

    Saving changes.

--- client/src/Components/Overview/Selection.jsx
+++ client/src/Components/Overview/Selection.jsx
@@ -2,22 +2,35 @@ import React, { useState, useContext, useEffect } from 'react';
 import currentStyle from '../../Contexts/CurStyleContext.js';
 
 function Selection({ }) {
-  const [photos, setPhotos] = useState([]);
   const [skus, setSkus] = useState({});
   const { curStyle } = useContext(currentStyle);
+  const [quant, setQuant] = useState(0);
+  const [loaded, setLoad] = useState(false);
+
+  const skuKeys = Object.keys(skus);
+
+  useEffect(() => {
+    if (curStyle.style_id) {
+      setSkus(curStyle.skus);
+      // setQuant(skus[skuKeys[0]].quantity);
+    }
+  }, [curStyle]);
+
+  function sizeSelect(e) {
+    setQuant(e.target.value);
+  }
 
   return (
     <div>
-      {console.log(curStyle)}
       <form>
         <div id="size">
-          <select>
-            <option value="size">Small</option>
+          <select onChange={(e) => sizeSelect(e)}>
+            {skuKeys.map((sku) => <option key={sku} value={sku}>{skus[sku].size}</option>)}
           </select>
         </div>
         <div id="quantity">
           <select>
-            <option value="quant">1</option>
+            <option value="quant">Quantity</option>
           </select>
         </div>
         <input type="button" value="Add to bag" />
