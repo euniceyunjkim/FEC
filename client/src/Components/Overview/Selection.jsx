5773b0b6f99b119df9d5a22208bce30199e9983e
Eunice Kim <euniceyunjkim@gmail.com>
2022-04-04 16:34:20 -0700

    Minor changes to selection component.

--- client/src/Components/Overview/Selection.jsx
+++ client/src/Components/Overview/Selection.jsx
@@ -1,41 +1,59 @@
 import React, { useState, useContext, useEffect } from 'react';
 import currentStyle from '../../Contexts/CurStyleContext.js';
+import styled from 'styled-components';
+
+const DropdownContainer = styled.div`
+display: flex;
+`;
+
+const SubmitContainer = styled.div`
+display: flex;
+`;
 
 function Selection({ }) {
-  const [skus, setSkus] = useState({});
+  const [skus, setSkus] = useState([]);
   const { curStyle } = useContext(currentStyle);
   const [quant, setQuant] = useState(0);
-  const [loaded, setLoad] = useState(false);
-
-  const skuKeys = Object.keys(skus);
 
   useEffect(() => {
     if (curStyle.style_id) {
-      setSkus(curStyle.skus);
-      // setQuant(skus[skuKeys[0]].quantity);
+      setSkus(Object.keys(curStyle.skus));
     }
   }, [curStyle]);
 
+  useEffect(() => {
+    if (skus) {
+      setQuant(curStyle.skus[skus[0]].quantity);
+    }
+  }, [skus]);
+
   function sizeSelect(e) {
-    setQuant(e.target.value);
+    setQuant(curStyle.skus[e.target.value].quantity);
   }
 
   return (
     <div>
       <form>
-        <div id="size">
-          <select onChange={(e) => sizeSelect(e)}>
-            {skuKeys.map((sku) => <option key={sku} value={sku}>{skus[sku].size}</option>)}
-          </select>
-        </div>
-        <div id="quantity">
-          <select>
-            <option value="quant">Quantity</option>
-          </select>
-        </div>
-        <input type="button" value="Add to bag" />
+        {console.log(curStyle)}
+        <DropdownContainer>
+          <div id="size">
+            <select onChange={(e) => sizeSelect(e)}>
+              {curStyle.skus.map((sku) =>
+                <option key={sku} value={sku}>{curStyle.skus[sku].size}</option>)}
+            </select>
+          </div>
+          <div id="quantity">
+            <select>
+              {console.log(quant)}
+              <option value="quant">{quant}</option>
+            </select>
+          </div>
+        </DropdownContainer>
+        <SubmitContainer>
+          <input type="button" value="Add to bag" />
+          <button type="button">Star</button>
+        </SubmitContainer>
       </form>
-      <button type="button">Star</button>
     </div>
   );
 }
