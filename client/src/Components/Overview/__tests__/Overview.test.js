c6469b42747b4561a23c581767acf38c82834a99
Eunice Kim <euniceyunjkim@gmail.com>
2022-04-05 23:00:01 -0700

    Completed initial testing for logo and cart in the Overview Component.

--- client/src/Components/Overview/__tests__/Overview.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { render, screen } from "@testing-library/react";
-import Overview from '../index.jsx';
-
-describe('App Component', () => {
-  it('renders Overview Component', () => {
-
-  });
-});
