d67bb72d4654f821a91128161e1a390d89b7eb31
Casey Eads <eadscaseyl@gmail.com>
2022-04-07 23:25:20 -0700

    Update format to better suit the requirements.

--- client/src/Components/QA/Components/Questions.jsx
+++ client/src/Components/QA/Components/Questions.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 function Questions({ question }) {
+  function addHelpful(qId) {
+    axios.put(`qa/questions/${qId}/helpful`, {});
+  }
+
   return (
     <div className="question">
-      <span>Q: {question.question_body}</span>
+      <span>{question.question_body}</span>
       <span>Helpful?</span>
-      <span>Yes ({question.question_helpfulness}) |</span>
+      <span onClick={() => addHelpful(question.question_id)} onKeyUp={() => addHelpful(question.question_id)}>Yes ({question.question_helpfulness}) |</span>
       <span> Add Answer</span>
     </div>
   );
