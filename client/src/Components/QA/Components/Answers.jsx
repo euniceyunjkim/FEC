1c50e5cb209b739c46fa280f99cb2e2bd3016151
Casey Eads <eadscaseyl@gmail.com>
2022-04-07 23:22:43 -0700

    Add functionality to yes and report. Include correct date format and user/seller.

--- client/src/Components/QA/Components/Answers.jsx
+++ client/src/Components/QA/Components/Answers.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import axios from 'axios';
+import moment from 'moment';
 
 function Answers({ answer }) {
+  function addHelpful(aId) {
+    axios.put(`qa/answers/${aId}/helpful`, {});
+  }
+  function addReport(aId) {
+    axios.put(`qa/answers/${aId}/report`, {});
+  }
+
   return (
-    <div>
-      <div>A: {answer.body}</div>
+    <div className="answer">
+      <div>{answer.body}</div>
       <div>
-        <span>by user, date |</span>
-        <span> Helpful? Yes (2) |</span>
-        <span> Report</span>
+      {answer.answerer_name.toLowerCase() === "seller" ? (<span>by <strong>{answer.answerer_name}</strong>, {moment(answer.date).format('MMMM Do YYYY')}|</span>) : (<span>by {answer.answerer_name}, {moment(answer.date).format('MMMM Do YYYY')}|</span>)}
+        <span> Helpful? </span>
+        <a onClick={() => addHelpful(answer.id)} onKeyUp={() => addHelpful(answer.id)}> Yes ({answer.helpfulness}) |</a>
+        <span onClick={() => addReport(answer.id)} onKeyUp={() => addReport(answer.id)}> Report</span>
       </div>
     </div>
   );
