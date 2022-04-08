1e6ad33d7d2ec76ee9698b6c826a4045bd61c2a5
Casey Eads <eadscaseyl@gmail.com>
2022-04-07 23:23:53 -0700

    Refactor for changes made to both questions and answers.

--- client/src/Components/QA/Components/QAList.jsx
+++ client/src/Components/QA/Components/QAList.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Questions from "./Questions.jsx";
 import Answers from "./Answers.jsx";
 
 function QAList({ question }) {
+  const [answers, setAnswers] = useState([question.answers[0], question.answers[1]]);
+  const [upTo, setUpTo] = useState(4)
+  // setAnswers(question.answers.slice(0, 2));
+
+  function addAnswers() {
+    setAnswers(question.answers.slice(0, upTo));
+    setUpTo(upTo + 2);
+  }
+
+  function collapse() {
+    setAnswers(question.answers.slice(0, 2));
+  }
+
   return (
     <div>
-      <Questions question={question}/>
-      { Object.entries(question.answers).map(([key, value]) => <Answers answer={value} />)}
-      {/* {localStorage.length > questions.length ? <span onClick="something">LOAD MORE ANSWERS</span> : <></> } */}
+      <span>Q: </span>
+      <div>
+        <Questions question={question} />
+      </div>
+      <span>A: </span>
+      <div>
+        { answers.map((answer) => <Answers answer={answer[1]} />)}
+        { answers.length < question.answers.length ? <div onClick={addAnswers}>See more answers</div> : answers.length > 2 ? <div onClick={collapse}>Collapse answers</div> : null}
+      </div>
     </div>
   );
 }
