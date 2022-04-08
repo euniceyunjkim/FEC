7f732b81834afba4cb6e6f8d6c4805ebee328fb6
Casey Eads <eadscaseyl@gmail.com>
2022-04-07 23:26:05 -0700

    Implement search functionality for 3 or more characters.

--- client/src/Components/QA/Components/Search.jsx
+++ client/src/Components/QA/Components/Search.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Search({}) {
+function Search({ questions, setQuestions, allQuestions }) {
+  const [searching, updateSearching] = useState('');
+
+  function updateSearch(e) {
+    updateSearching(e.target.value);
+    if (e.target.value.length >= 3) {
+      setQuestions(allQuestions.filter((question) => question.question_body.toLowerCase().includes(e.target.value.toLowerCase())));
+    } else {
+      setQuestions(allQuestions);
+    }
+  }
+
+  // question.question_body.toLowerCase().contains(e.target.value)
+
   return (
     <form>
-      <input type="text" />
+      <input type="text" value={searching} placeholder="Have a question? Search for answers..." onChange={updateSearch} />
     </form>
   );
 }
