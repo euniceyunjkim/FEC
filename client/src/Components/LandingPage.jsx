cfa0a1081e201dd7e264a91a56c0651d1d6810d7
Eunice Kim <euniceyunjkim@gmail.com>
2022-04-11 23:21:35 -0700

    Created new style file for the Landing Page component. Fixed some linter warnings.

--- client/src/Components/LandingPage.jsx
+++ client/src/Components/LandingPage.jsx
@@ -1,106 +1,47 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import {
+  Page, BgContainer, Background, AnimLogo,
+  Joestar, Ready,
+} from './StyledComps/LandingPageStyle';
 
-const Page = styled.div`
-background-color: black;
-`;
-const BgContainer = styled.div`
-display: flex;
-justify-content: center;
-`;
-
-const Background = styled.div`
-height: 1050px;
-width: 1680px;
-display: grid;
-grid-template: 1fr/ 1fr;
-background-image: ${({ src }) => `url(${src})`};
-background-repeat: no-repeat;
-background-size: contain;
-position: relative;
-place-items: center;
-margin: auto;
-`;
-
-const AnimLogo = styled.div`
-z-index: 1;
-position: absolute;
-background-image: ${({ src }) => `url(${src})`};
-background-repeat: no-repeat;
-background-size: contain;
-width: 75%;
-height: 75%;
-`;
-
-const Joestar = styled.button`
-z-index: 2;
-position: absolute;
-top: 70%;
-left: 42.25%;
-color: black;
-border: 1px solid;
-background-color: transparent;
-font-align: center;
-font-size: 20px;
-padding:10px 21px;
-margin: 0 auto;
-font-weight: bold;
-font-family: 'Amatic SC', cursive;
-font-family: 'Space Mono', monospace;
-
-&:hover {
-  cursor: pointer;
-  background-color: #4b15a3;
-  color: white;
-};
-`;
-
-const Ready = styled.button`
-z-index: 2;
-position: absolute;
-top: 70%;
-left: 47%;
-color: black;
-border: 1px solid;
-background-color: transparent;
-font-align: center;
-font-size: 20px;
-padding:10px 21px;
-margin: 0 auto;
-font-weight: bold;
-font-family: 'Amatic SC', cursive;
-font-family: 'Space Mono', monospace;
-
-&:hover {
-  cursor: pointer;
-  background-color: #4b15a3;
-  color: white;
-};
-`;
-
-let LandingPage = ({ }) => {
-  let navigate = useNavigate();
+function LandingPage() {
+  const navigate = useNavigate();
   const [clicked, setClick] = useState(false);
 
   function playMusic() {
     setClick(true);
-    const audio = document.getElementById("bgmusic");
+    const audio = document.getElementById('bgmusic');
     audio.play();
   }
+
+  const playMusicCB = useCallback(() => playMusic(), []);
 
   return (
     <Page>
       <BgContainer>
         <Background src="overview_imgs/Jojoland.jpeg">
           <AnimLogo src="overview_imgs/AnimLogo.gif" />
-          {clicked ? <Joestar onClick={() => { navigate("/40346") }}>START YOUR ADVENTURE
-          </Joestar> : <Ready onClick={playMusic}>READY?</Ready>}
-          <audio id="bgmusic" src='overview_imgs/audio.mp3' type="audio/mp3" />
+          {clicked ? (
+            <Joestar onClick={() => { navigate('/40346'); }}>
+              START YOUR ADVENTURE
+            </Joestar>
+          ) : (
+            <Ready onClick={playMusicCB}>
+              READY?
+            </Ready>
+          )}
+          <audio
+            id="bgmusic"
+            src="overview_imgs/audio.mp3"
+            type="audio/mp3"
+          >
+            <track kind="captions" />
+          </audio>
         </Background>
       </BgContainer>
     </Page>
-  )
+  );
 }
 
 export default LandingPage;
