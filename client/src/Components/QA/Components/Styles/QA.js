edd3ce6278a3a0bd0a04591a8d341f28f59d1388
Casey Eads <eadscaseyl@gmail.com>
2022-04-12 23:12:01 -0700

    Styling for Question and Answering display.

---
+++ client/src/Components/QA/Components/Styles/QA.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const QAListStyle = styled.div`
+  padding: 20px;
+  display: flex;
+  align-items: left;
+  align-self: center;
+  flex-direction: column;
+  justify-content: start;
+  width: 80%;
+  height: 50%;
+  overflow: auto;
+`;
+
+export const QAStyle = styled.div`
+  height: 80%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export const ParentQ = styled.div`
+  display: flex
+`;
+
+export const QStyle = styled.div`
+  display: inline;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px;
+`;
+
+export const QFeat = styled.div`
+  display: inline;
+  padding-right: 5px;
+`;
+
+export const QFeatures = styled.div`
+  display: inline;
+  margin-left: auto;
+  color: grey;
+  padding-top: 13px;
+  vertical-align: top;
+`;
