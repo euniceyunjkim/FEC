b4683d2fff656c2214c474c8bd5c7d86a2263f05
Alex Shiao <alexshiao99@gmail.com>
2022-04-15 11:59:16 -0700

    Improved accesibility thru overview search bar

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use('/cart', CartRouters);
 app.use('/interactions', InteractionRouters);
 app.use('/newoutfit', NewOutfitRouters);
 app.get('/robots.txt', (req, res) => {
-  res.sendFile(path.join(__dirname, 'robot.txt'));
+  res.sendFile(path.join(__dirname, 'robots.txt'));
 });
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
